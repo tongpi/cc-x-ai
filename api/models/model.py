45fced3f278358feeeecfd88cbf94acce0dd6476
Hickays <Hickays@163.com>
2024-02-04 05:45:48 +0000

    fix: 空间仅显示当前用户创建的应用(新增表字段)

--- api/models/model.py
+++ api/models/model.py
@@ -27,7 +27,8 @@ class App(db.Model):
     __tablename__ = 'apps'
     __table_args__ = (
         db.PrimaryKeyConstraint('id', name='app_pkey'),
-        db.Index('app_tenant_id_idx', 'tenant_id')
+        db.Index('app_tenant_id_idx', 'tenant_id'),
+        db.Index('app_account_id_idx', 'account_id')
     )
 
     id = db.Column(UUID, server_default=db.text('uuid_generate_v4()'))
@@ -47,6 +48,8 @@ class App(db.Model):
     is_universal = db.Column(db.Boolean, nullable=False, server_default=db.text('false'))
     created_at = db.Column(db.DateTime, nullable=False, server_default=db.text('CURRENT_TIMESTAMP(0)'))
     updated_at = db.Column(db.DateTime, nullable=False, server_default=db.text('CURRENT_TIMESTAMP(0)'))
+
+    account_id = db.Column(UUID, nullable=False)
 
     @property
     def site(self):
@@ -68,7 +71,7 @@ class App(db.Model):
     def tenant(self):
         tenant = db.session.query(Tenant).filter(Tenant.id == self.tenant_id).first()
         return tenant
-    
+
     @property
     def is_agent(self) -> bool:
         app_model_config = self.app_model_config
@@ -77,10 +80,10 @@ class App(db.Model):
         if not app_model_config.agent_mode:
             return False
         if self.app_model_config.agent_mode_dict.get('enabled', False) \
-            and self.app_model_config.agent_mode_dict.get('strategy', '') in ['function_call', 'react']:
+                and self.app_model_config.agent_mode_dict.get('strategy', '') in ['function_call', 'react']:
             return True
         return False
-    
+
     @property
     def deleted_tools(self) -> list:
         # get agent mode tools
@@ -91,7 +94,7 @@ class App(db.Model):
             return []
         agent_mode = app_model_config.agent_mode_dict
         tools = agent_mode.get('tools', [])
-        
+
         provider_ids = []
 
         for tool in tools:
@@ -127,6 +130,7 @@ class App(db.Model):
                     deleted_tools.append(tool['tool_name'])
 
         return deleted_tools
+
 
 class AppModelConfig(db.Model):
     __tablename__ = 'app_model_configs'
@@ -413,6 +417,7 @@ class InstalledApp(db.Model):
         if not app:
             return False
         return app.is_agent
+
 
 class Conversation(db.Model):
     __tablename__ = 'conversations'
@@ -725,6 +730,7 @@ class MessageFile(db.Model):
     created_by = db.Column(UUID, nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, server_default=db.text('CURRENT_TIMESTAMP(0)'))
 
+
 class MessageAnnotation(db.Model):
     __tablename__ = 'message_annotations'
     __table_args__ = (
@@ -1031,7 +1037,7 @@ class MessageAgentThought(db.Model):
             return json.loads(self.message_files)
         else:
             return []
-        
+
     @property
     def tool_labels(self) -> dict:
         try:
@@ -1041,6 +1047,7 @@ class MessageAgentThought(db.Model):
                 return {}
         except Exception as e:
             return {}
+
 
 class DatasetRetrieverResource(db.Model):
     __tablename__ = 'dataset_retriever_resources'
